3677768f2452a8b0f3f5d37712afc21e1731d611
mahmoudAllam <mahsha123@gmail.com>
2019-03-14 21:24:50 +0200

    Change zendesk account key.

--- src/Helpers.js
+++ src/Helpers.js
@@ -6,7 +6,7 @@ import { strings } from './i18n';
 import Toast from 'react-native-root-toast';
 import colors from './theme/colors';
 
-export const ZOPIM_ACCOUNT_KEY = '6NFj0gv0sApOZnoEu2t4JRQssHYXoB1q';
+export const ZOPIM_ACCOUNT_KEY = 'pRVfyd50YAOsuwoV7WHrDXi5UBLmKE78';
 
 export const isIOS = () => Platform.OS === 'ios';
 
