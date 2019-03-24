0fc2939b0b048f98a81e1021e83120647065cdc2
mahmoudAllam <mahsha123@gmail.com>
2019-03-24 12:53:21 +0200

    Add toast to chat ios.

--- src/Helpers.js
+++ src/Helpers.js
@@ -6,7 +6,9 @@ import { strings } from './i18n';
 import Toast from 'react-native-root-toast';
 import colors from './theme/colors';
 
-export const ZOPIM_ACCOUNT_KEY = __DEV__ ? '6NFj0gv0sApOZnoEu2t4JRQssHYXoB1q' : 'pRVfyd50YAOsuwoV7WHrDXi5UBLmKE78';
+export const ZOPIM_ACCOUNT_KEY = __DEV__
+  ? '6NFj0gv0sApOZnoEu2t4JRQssHYXoB1q'
+  : 'pRVfyd50YAOsuwoV7WHrDXi5UBLmKE78';
 
 export const isIOS = () => Platform.OS === 'ios';
 
@@ -19,9 +21,9 @@ export const toast = str => {
   Toast.show(str, {
     shadow: true,
     animation: true,
-    hideOnPress: true,
-    backgroundColor: colors.white,
-    textColor: colors.black
+    hideOnPress: true
+    // backgroundColor: colors.white,
+    // textColor: colors.black
   });
 };
 
