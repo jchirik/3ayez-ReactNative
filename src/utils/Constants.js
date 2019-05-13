6709199cb2aff0526f66048d62961b57ac72485d
Khattab <ahmedibraheem93@gmail.com>
2019-05-13 20:09:36 +0200

    Force user to update out-of-dated version CST-211

---
+++ src/utils/Constants.js
@@ -0,0 +1,15 @@
+const APP_STORE_LINK = 'itms-apps://itunes.apple.com/eg/app/id1329892544?mt=8';
+const APP_STORE_URL = 'https://itunes.apple.com/eg/app/id1329892544?mt=8';
+
+const PLAY_STORE_LINK = 'market://details?id=com.ayez.ayezcustomer';
+const PLAY_STORE_URL =
+  'https://play.google.com/store/apps/details?id=com.ayez.ayezcustomer';
+
+const constants = {
+  APP_STORE_LINK,
+  APP_STORE_URL,
+  PLAY_STORE_LINK,
+  PLAY_STORE_URL
+};
+
+export default constants;
