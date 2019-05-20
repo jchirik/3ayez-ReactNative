37d5244c1eaaccf00d112612f5f320ee0f2cc59c
mahmoudAllam <mahsha123@gmail.com>
2019-05-20 14:04:12 +0200

    Remove send build context to sentry.

--- src/utils/sentryUtils.js
+++ src/utils/sentryUtils.js
@@ -5,19 +5,7 @@ import Config from 'react-native-config';
 
 export function setupSentry() {
   Sentry.config(Config.SENTRY_DSN).install();
-  addBuildContext();
 }
 
 export const ravenMiddleware = createRavenMiddleware(Sentry);
 
-const addBuildContext = () => {
-  Sentry.setTagsContext({
-    appVersion: DeviceInfo.getVersion(),
-    buildNumber: DeviceInfo.getBuildNumber(),
-    deviceInfo: {
-      systemName: DeviceInfo.getSystemName(),
-      systemVersion: DeviceInfo.getSystemVersion(),
-      deviceName: DeviceInfo.getDeviceName()
-    }
-  });
-};
